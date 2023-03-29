1a9c6267781fdc48e46ad34b398bfd699ea002c7
1850 <1850184401@qq.com>
2023-03-29 17:18:46 +0800

    制作CopyUtil封装BeanUtil

--- src/main/java/com/example/wiki/controller/EBookController.java
+++ src/main/java/com/example/wiki/controller/EBookController.java
@@ -1,7 +1,9 @@
 package com.example.wiki.controller;
 
 import com.example.wiki.domain.Ebook;
+import com.example.wiki.req.EBookReq;
 import com.example.wiki.resp.CommonResp;
+import com.example.wiki.resp.EBookResp;
 import com.example.wiki.service.EBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,9 @@ public class EBookController {
     private EBookService eBookService;
 
     @GetMapping("/list")
-    public CommonResp<List<Ebook>> list(){
-        CommonResp<List<Ebook>> commonResp = new CommonResp();
-        List<Ebook> list =  eBookService.select();
+    public CommonResp list(EBookReq param){
+        CommonResp<List<EBookResp>> commonResp = new CommonResp();
+        List<EBookResp> list =  eBookService.select(param);
         commonResp.setContent(list);
         return commonResp;
     }
--- src/main/java/com/example/wiki/service/EBookService.java
+++ src/main/java/com/example/wiki/service/EBookService.java
@@ -1,22 +1,30 @@
 package com.example.wiki.service;
 
 import com.example.wiki.domain.Ebook;
+import com.example.wiki.domain.EbookExample;
 import com.example.wiki.domain.Test;
 import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.mapper.TestMapper;
+import com.example.wiki.req.EBookReq;
+import com.example.wiki.resp.EBookResp;
+import com.example.wiki.util.CopyUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class EBookService {
         @Autowired
         private EbookMapper ebookMapper;
 
-        public List<Ebook> select(){
-            List<Ebook> users = ebookMapper.selectByExample(null);
-            return users;
+        public List<EBookResp> select(EBookReq param){
+            EbookExample ebookExample = new EbookExample();
+            final EbookExample.Criteria criteria = ebookExample.createCriteria();
+            criteria.andNameLike("%" + param.getName() + "%");
+            List<Ebook> users = ebookMapper.selectByExample(ebookExample);
+            final List<EBookResp> eBookResps = CopyUtil.copyList(users, EBookResp.class);
+            return eBookResps;
         };
-
-
 }
